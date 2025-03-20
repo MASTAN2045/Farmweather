7dba53d8f0753299d180365cd42c2f61b27c99c9
MASTAN2045 <mastanraobusiraju000@gmail.com>
2025-03-20 15:41:31 +0530

    update mongodb2

--- backend/server.js
+++ backend/server.js
@@ -8,10 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // MongoDB Connection
-mongoose.connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGODB_URI)
 .then(() => console.log('Connected to MongoDB'))
 .catch(err => console.error('MongoDB connection error:', err));
 
